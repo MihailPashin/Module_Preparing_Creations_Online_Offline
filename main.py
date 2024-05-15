110c6baaad326bb1ced18ed603c5f968485a2654
MihailPashin <michalpoisonish@gmail.com>
2024-05-15 16:57:37 +0300

    Moving a early version in another repo

--- main.py
+++ main.py
@@ -13,23 +13,19 @@ if __name__ == "__main__" :
     print(f' type of keywords = {type(keywords)}')
     print(keywords[0:3])
     
-    full_dataset = list(Ready_DataFrame.df['message'])
-    print(full_dataset[0:3])
-    print(f' type of full_dataset = {type(full_dataset)}')
+    #full_dataset = list(Ready_DataFrame.df['message'])
+    #print(full_dataset[0:3])
+    #print(f' type of full_dataset = {type(full_dataset)}')
     
-    word2vec_model_tiny = Word2VecModel()
-    word2vec_model_large = Word2VecModel('Word2Vec/preloaded_model/14cities.model')
-    train_tokens = word2vec_model_tiny.tokenize_corpus(full_dataset)
+    #word2vec_model_tiny = Word2VecModel()
+    #word2vec_model_large = Word2VecModel('Word2Vec/preloaded_model/14cities.model')
+    #train_tokens = word2vec_model_tiny.tokenize_corpus(full_dataset)
 
     result=[]
-    for wv_model in [word2vec_model_tiny,word2vec_model_large]:
-        if wv_model.model is None:
-            print('Создаём схему')
-            wv_model.creating_word2vec_model(train_tokens)
-        result.append(wv_model.filter(dict_for_razmetka))
-    print(f'len {len(result)}')
-    print(f'len {len(result[0])}')
-    print(f'len {len(result[1])}')
+
+    #print(f'len {len(result)}')
+    #print(f'len {len(result[0])}')
+    #print(f'len {len(result[1])}')
    
     model_ruBERT = BERT_Process()
     model_ruBERT.preprocess_yake_phrases(keywords)
@@ -37,7 +33,7 @@ if __name__ == "__main__" :
     result.append(model_ruBERT.summarization_indexes(dict_for_razmetka))
     
     print(f'len {len(result)}')
-    print(f'len {len(result[0])}')
-    print(f'len {len(result[1])}')
-    print(f'len {len(result[2])}')
-    print(f'result from bolnichka {result[2][0]}')
+    #print(f'len {len(result[0])}')
+    #print(f'len {len(result[1])}')
+    #print(f'len {len(result[2])}')
+    print(f'result from bolnichka {result[0][0]}')
