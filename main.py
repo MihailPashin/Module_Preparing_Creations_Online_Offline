cd51469eb972f25480199e0ebf965e0c626a674e
MihailPashin <michalpoisonish@gmail.com>
2024-05-17 15:26:32 +0300

    Исправлен класс Pandas-ECB. Название районов отображаются.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from Yake_KeyWords_Extract.yake_keywords import YakeExtractor, YakeBoundary, Yak
 from RuBERT.RuBERT_ECB import ruBERT, BERT_Process
 from XLM_RoBERTa.Sentiment_Analysis_ECB import SentimentModel,SentimentModel_Process
 from Nested_List_to_JSON.save_to_json import NestedListToJSON
+from Preparing_Tables.Grouping_Summary import DataProcessor, DataBoundary, DataController
+import pandas as pd
 
 if __name__ == "__main__" :
 
@@ -15,15 +17,18 @@ if __name__ == "__main__" :
     group_interface.display_groups() ## Отобразил все тематики
 
     filepath = 'Loading_DataSet/data/New_coordinates_titles.csv'
-    df_entity = DataFrameEntity(filepath)
-    df_control = DataFrameControl(df_entity)
-    df_control.process_data()
-
+    df_boundary = DataFrameBoundary(filepath)
+    df_boundary.controller.process_data()
+    reviews = df_boundary.get_reviews('message')
+    entire_df = df_boundary.get_all_dataframe()
+    print(f' Тип данных переменной result - ', {type(entire_df)})
+    print(f' Число строк result - ', {len(entire_df)})
+    '''
     df_boundary = DataFrameBoundary(df_entity)
-    reviews = df_boundary.get_reviews('message') ## Получил все отзывы
+    reviews = df_boundary.get_reviews('message') ## Получил все отзывы. Верно - актор может общаться с интерфейсом
     
     yake_boundary = YakeBoundary()
-    keywords = yake_boundary.get_keywords(reviews) ## Проверка отзывов на словарь + KeyPhraseExtraction 
+    keywords = yake_boundary.get_keywords(reviews) ## Проверка отзывов на словарь + KeyPhraseExtraction. - актор может общаться с интерфейсом
     print(f' type of keywords = {type(keywords)}', len(keywords))
     
     #converter = NestedListToJSON(keywords) ## Сохранение в JSON формат для теста
@@ -41,3 +46,19 @@ if __name__ == "__main__" :
     result = sentiment_model.predict_tonalnost(list_by_groups, dict_for_razmetka)
     print('Sentiment Analysis окончен. Число строк в таблице', len(result))    
     print(f' Тип данных переменной result - ', {type(result)})
+
+    sliced_df = result.head(5)
+    sliced_df = pd.concat([sliced_df, result.tail(5)])
+
+    print ('resulting_dataframe',sliced_df)
+    
+    data_processor = DataProcessor()
+    data_controller = DataController(data_processor)
+    data_boundary = DataBoundary(data_controller)
+    all_dataset = df_boundary.get_all_dataframe()
+    df_entity.display_info()
+    processed_data = data_boundary.validate_and_process(result, all_dataset)
+    sliced_df = processed_data.head(5)
+    sliced_df = pd.concat([sliced_df, processed_data.tail(5)])
+    print('Транспонированная таблица', sliced_df)
+    '''
