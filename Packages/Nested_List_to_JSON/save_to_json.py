57d09c9ebf2f2ca5c2a12f6369a8681305a5535f
MihailPashin <michalpoisonish@gmail.com>
2024-05-18 18:17:23 +0300

    Перенесен весь функционал из Colab в классы согласно паттерну Entity-Control-Boundary

--- Packages/Nested_List_to_JSON/save_to_json.py
+++ Packages/Nested_List_to_JSON/save_to_json.py
@@ -4,9 +4,9 @@ class NestedListToJSON:
     def __init__(self, nested_list):
         self.nested_list = nested_list
     
-    def save_to_json(self, file_path):
+    def save_to_json(self, folder_name, jsons):
         try:
-            with open(file_path, 'w',encoding='utf-8') as json_file:
-                json.dump(self.nested_list, json_file, indent=4)
+            with open("{}/{}.json".format(folder_name,jsons), 'w',encoding='utf-8') as json_file:
+                json.dump(self.nested_list, json_file, ensure_ascii=False)
         except Exception as e:
             print(f"An error occurred: {e}")
